dd05d8ed6842d0b5482fa4d362fc0782102e00be
Jake <jakewmayfield@gmail.com>
2022-01-11 16:50:20 -0600

    js fun

--- JS Camera App_1/main.js
+++ JS Camera App_1/main.js
@@ -13,6 +13,7 @@ let selected = -1; // Default no filter
  * **p5.js** library automatically executes the `preload()` function. Basically, it is used to load external files. In our case, we'll use it to load the images for our filters and assign them to separate variables for later use.
 */
 function preload()
+
 {
   // Spiderman Mask Filter asset
   imgSpidermanMask = loadImage("https://i.ibb.co/9HB2sSv/spiderman-mask-1.png");
@@ -21,6 +22,14 @@ function preload()
   imgDogEarRight = loadImage("https://i.ibb.co/bFJf33z/dog-ear-right.png");
   imgDogEarLeft = loadImage("https://i.ibb.co/dggwZ1q/dog-ear-left.png");
   imgDogNose = loadImage("https://i.ibb.co/PWYGkw1/dog-nose.png");
+
+  // Khaled & Henry assets
+  imgBemused=loadImage("bemused1.png")
+  imgDisgust=loadImage("disgust.png")
+  // imgExpository=loadImage("expository1.png")
+  imgHenry=loadImage("Henry Afraid Henry Very Afraid.png")
+  // imgScared=loadImage("scared1.png")
+  imgInquisitive=loadImage("what_are_you_doing.png")
 }
 
 /**
@@ -42,7 +51,7 @@ function setup()
 
   // select filter
   const sel = createSelect();
-  const selectList = ['Spiderman Mask', 'Dog Filter']; // list of filters
+  const selectList = ['Spiderman Mask', 'Dog Filter', 'Bemused', 'Disgust', 'Inquisitive', 'Henry']; // list of filters
   sel.option('Select Filter', -1); // Default no filter
   for (let i = 0; i < selectList.length; i++)
   {
@@ -75,6 +84,13 @@ function draw()
     case '-1': break;
     case '0': drawSpidermanMask(); break;
     case '1': drawDogFace(); break;
+    case '2': drawBemused(); break;
+    case '3': drawDisgust(); break;
+    // case '4': drawExpository(); break;
+    case '4': drawInquisitive(); break;
+    // case '6': drawScared(); break;
+    case '5': drawHenry(); break;
+
   }
 }
 
@@ -92,6 +108,97 @@ function drawSpidermanMask()
     pop();
   }
 }
+
+// Bemused
+function drawBemused()
+{
+  const positions = faceTracker.getCurrentPosition();
+  if (positions !== false)
+  {
+    push();
+    const wx = Math.abs(positions[13][0] - positions[1][0]) * 1.2; // The width is given by the face width, based on the geometry
+    const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.5; // The height is given by the distance from nose to chin, times 2
+    translate(-wx/2, -wy/2);
+    image(imgBemused, positions[62][0], positions[63][1], wx, wy); // Show the mask at the center of the face
+    pop();
+  }
+}
+
+// Disgust
+function drawDisgust()
+{
+  const positions = faceTracker.getCurrentPosition();
+  if (positions !== false)
+  {
+    push();
+    const wx = Math.abs(positions[13][0] - positions[1][0]) * 1.5; // The width is given by the face width, based on the geometry
+    const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 2; // The height is given by the distance from nose to chin, times 2
+    translate(-wx/2, -wy/2);
+    image(imgDisgust, positions[62][0], positions[62][1], wx, wy); // Show the mask at the center of the face
+    pop();
+  }
+}
+
+// // Expository
+// function drawExpository()
+// {
+//   const positions = faceTracker.getCurrentPosition();
+//   if (positions !== false)
+//   {
+//     push();
+//     const wx = Math.abs(positions[13][0] - positions[1][0]) * 2; // The width is given by the face width, based on the geometry
+//     const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.5; // The height is given by the distance from nose to chin, times 2
+//     translate(-wx/2, -wy/2);
+//     image(imgExpository, positions[62][0], positions[62][1], wx, wy); // Show the mask at the center of the face
+//     pop();
+//   }
+// }
+
+// Inquisitive
+function drawInquisitive()
+{
+  const positions = faceTracker.getCurrentPosition();
+  if (positions !== false)
+  {
+    push();
+    const wx = Math.abs(positions[13][0] - positions[1][0]) * 1.2; // The width is given by the face width, based on the geometry
+    const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.5; // The height is given by the distance from nose to chin, times 2
+    translate(-wx/2, -wy/2);
+    image(imgInquisitive, positions[62][0], positions[62][1], wx, wy); // Show the mask at the center of the face
+    pop();
+  }
+}
+
+// Scared
+// function drawScared()
+// {
+//   const positions = faceTracker.getCurrentPosition();
+//   if (positions !== false)
+//   {
+//     push();
+//     const wx = Math.abs(positions[13][0] - positions[1][0]) * 1.2; // The width is given by the face width, based on the geometry
+//     const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.5; // The height is given by the distance from nose to chin, times 2
+//     translate(-wx/2, -wy/2);
+//     image(imgScared, positions[62][0], positions[62][1], wx, wy); // Show the mask at the center of the face
+//     pop();
+//   }
+// }
+
+// Henry
+function drawHenry()
+{
+  const positions = faceTracker.getCurrentPosition();
+  if (positions !== false)
+  {
+    push();
+    const wx = Math.abs(positions[13][0] - positions[1][0]) * 3; // The width is given by the face width, based on the geometry
+    const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 2.5; // The height is given by the distance from nose to chin, times 2
+    translate(-wx/2, -wy/2);
+    image(imgHenry, positions[64][0], positions[64][1], wx, wy); // Show the mask at the center of the face
+    pop();
+  }
+}
+
 
 // Dog Face Filter
 function drawDogFace()
